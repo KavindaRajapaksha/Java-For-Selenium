102da7f482f2954c7119bae8030e13faa019c8b6
Kavinda <rdks.kavinda@gmail.com>
2024-05-17 22:59:32 +0530

    first commit

---
+++ src/day1/JavaFirstProgram.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class JavaFirstProgram {
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+            System.out.println("Kavinda");
+        }
+    }
+}
