8b3ca8c16e84b79af84f28cfcbd09aef0756902a
Kavinda <rdks.kavinda@gmail.com>
2024-05-30 19:42:08 +0530

    String class

---
+++ src/day8/StringMethod.java
@@ -0,0 +1,26 @@
+package day8;
+
+import java.util.Scanner;
+
+public class StringMethod {
+    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        String s1 = "Kavinda";
+//        String s2 = "Rajapaksha";
+//        System.out.print("Enter a string: ");
+//        String s3 = scanner.nextLine();
+//        System.out.println(s1 + " " + s2 + " " + s3);
+//        System.out.println(s1.concat(s2).concat(s3));//conctinate the string
+//        String s="                         W e L C o M E           kuhkh";
+//        String r="dyugffuy";
+//        System.out.println(s.length());
+//        System.out.println(s.trim());//remove the space
+//        System.out.println(s.trim().length());
+//        System.out.println(r.charAt(3));
+//        System.out.println(r.contains("yu"));
+
+
+
+
+    }
+}
