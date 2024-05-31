2742d957a6962cf92e1b74d692a252f420572ce6
Kavinda <rdks.kavinda@gmail.com>
2024-05-31 12:49:13 +0530

    String extend

--- src/day8/StringMethod.java
+++ src/day8/StringMethod.java
@@ -18,8 +18,29 @@ public class StringMethod {
 //        System.out.println(s.trim().length());
 //        System.out.println(r.charAt(3));
 //        System.out.println(r.contains("yu"));
+//        String s1="welcome";
+//        String s2="welcome";
+//
+//        System.out.println(s1.equals(s2));
+//        System.out.println(s1.equals("welcome"));
 
-
+//        String w="kavinda";
+//        System.out.println(w.replace('a','x')); //replcae character
+//        String s1="Kavinda";
+//
+//        for (int i=0;i<s1.length();i++){
+//            System.out.println(s1.charAt(i));
+//        }
+//  String name="Kavindas";
+//  System.out.println(name.substring(0,4)+name.substring(5));
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.toLowerCase());
+//        String email="rdks.kavinda@gmail.com";
+//        String a[]=email.split("@");
+//        System.out.println(a[0]);
+//        System.out.println(a[1]);
+//        String amount="$400,000";
+//        System.out.println(amount.replace("$","").replace(",",""));
 
 
     }
---
+++ src/day9/StringExamples.java
@@ -0,0 +1,8 @@
+package day9;
+
+public class StringExamples {
+     public static void main(String[] args){
+         System.out.println("Kavinda");
+     }
+
+}
