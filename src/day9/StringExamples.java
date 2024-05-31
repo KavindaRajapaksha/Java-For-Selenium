094c0ea1240d0c69713cd8d35459729a5cd47e95
Kavinda <rdks.kavinda@gmail.com>
2024-05-31 22:40:39 +0530

    String reverse

--- src/day9/StringExamples.java
+++ src/day9/StringExamples.java
@@ -2,7 +2,24 @@ package day9;
 
 public class StringExamples {
      public static void main(String[] args){
-         System.out.println("Kavinda");
+//         System.out.println("Kavinda");
+         //reverse the string
+         String s="Welcome";
+//         String rev="";
+//         for(int i=s.length()-1;i>=0;i--){
+//             rev+=s.charAt(i);
+//         }
+//         System.out.println(rev);
+//         char a[]=s.toCharArray();
+//         for(int i=a.length-1;i>=0;i--){
+//             System.out.print(a[i]);
+//         } //type 2
+
+  //using string buffer class
+//         StringBuffer s1= new StringBuffer("Kavinda");
+//         System.out.print("reverse string: "+ s1.reverse());
+
+
      }
 
 }
