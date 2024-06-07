8bce31bfb184486c60b2e61eea5ad876dce0a7e6
Kavinda <rdks.kavinda@gmail.com>
2024-06-08 00:12:42 +0530

    Exception Handling

---
+++ src/day18/UncheckedException.java
@@ -0,0 +1,41 @@
+package day18;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class UncheckedException {
+    public static void main(String[] args){
+
+        Scanner scanner=new Scanner(System.in);
+
+
+//  Example 1
+//        int[] a= new int[5];
+//
+//        System.out.println("Enter the pos :");
+//        int pos=scanner.nextInt();
+//        System.out.println("Enter the value: ");
+//        int value=scanner.nextInt();
+//
+//        a[pos]=value;
+//        System.out.println(a[pos]);//arrayIndexoutofbonds
+//            Example2
+//         String s="1234";
+//        System.out.println(Integer.parseInt(s)+10);//number format
+//
+//        Example3
+//        String name=null;
+//        System.out.println(name.length());//null pointer
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
