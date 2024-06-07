965488777be4b19bd98859c173ce7f522b7a9e22
Kavinda <rdks.kavinda@gmail.com>
2024-06-07 20:20:40 +0530

    data conversion

---
+++ src/day17/DataConversion.java
@@ -0,0 +1,14 @@
+package day17;
+
+public class DataConversion {
+    public static void main(String[] args){
+        String s="87687";
+        String s2="true";
+        System.out.println(Integer.parseInt(s));
+        System.out.println(Boolean.parseBoolean(s2));
+        System.out.println(Double.parseDouble(s));
+        int a=3939;
+        System.out.println(String.valueOf(a));
+
+    }
+}
