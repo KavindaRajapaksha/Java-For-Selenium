f4a5bb464a3b909f16dab44e73a4ab75a119ac59
Kavinda <rdks.kavinda@gmail.com>
2024-06-08 22:42:04 +0530

    type casting

---
+++ src/day19/TypeCastingDemo.java
@@ -0,0 +1,30 @@
+package day19;
+
+public class TypeCastingDemo {
+    public static void main(String[] args){
+//       int intValue=399;
+//       long lValue=intValue; //upcasting
+//        System.out.println(lValue);
+
+//        float fValue=1.22F;
+//        double dValue=fValue; //upcasting
+//        System.out.println(dValue);
+
+//         long lValue=10000;
+//         int iValue=(int)lValue; //downcasting
+//
+//        System.out.println(iValue);
+//    int i=100;
+//    double d=i;
+//        System.out.println(d);
+
+//        example2-cant do that
+//double d=10.22;
+//int i=(int)d;
+//        System.out.println(i);
+
+
+
+
+    }
+}
---
+++ src/day19/TypeCastingObject.java
@@ -0,0 +1,39 @@
+package day19;
+class Parent{
+    String name="Kavinda";
+
+    void m1(){
+        System.out.println(name);
+    }
+}
+
+class Child extends Parent{
+    int id=10;
+    void m2(){
+        System.out.println(id);
+    }
+}
+
+
+
+public class TypeCastingObject {
+    public static void main(String[] args) {
+//        Child c = new Child();
+//        System.out.println(c.name);
+//        c.m1();
+//        System.out.println(c.id);
+//        c.m2();
+
+//         Parent p=new Parent();
+//         Child c=(Child)p;
+//
+//        System.out.println(c.id);
+//        System.out.println(c.name); //downcasting
+
+
+
+
+
+
+    }
+}
